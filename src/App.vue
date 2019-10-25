c34ac19a27081b6f64099a32fb4a2c28292881f4
matia-gov <mohamed.atia@phila.gov>
2019-10-25 16:40:02 -0400

    linted

--- src/App.vue
+++ src/App.vue
@@ -7,55 +7,62 @@
         class="search-field"
         type="text"
         placeholder="Search by title, department, or keyword"
-
       ><input
         ref="archive-search-bar"
         type="submit"
         class="search-submit"
         value="Search"
-       
       >
     </div>
     <div id="programs-container">
       <div id="filters-container" />
       <div id="programs-display">
         <div id="tiles">
-           <paginate 
-          name="programs"
-          :list="programs"
-          id="program-results"
-          class="grid-x grid-margin-x paginate-list"
-          tag="div"
-          :per="16">
-            <div v-for="program in paginated('programs')"
+          <paginate 
+            id="program-results"
+            name="programs"
+            :list="programs"
+            class="grid-x grid-margin-x paginate-list"
+            tag="div"
+            :per="16"
+          >
+            <div
+              v-for="program in paginated('programs')"
               :key="program.id"
-              class="medium-12 cell mbl">
-              <a class="card program-card" :href="program.link">
-              <div class="trim"><img :src="program.image" alt="" class="program-image"/></div>
-              <div class="content-block">
-              <h3 :class="{ external: [program.link].includes('http') }">{{program.title}}</h3>
-                <p>{{program.short_description}}</p>
-              </div>
-            </a>
+              class="medium-12 cell mbl"
+            >
+              <a
+                class="card program-card"
+                :href="program.link"
+              >
+                <div class="trim"><img
+                  :src="program.image"
+                  alt=""
+                  class="program-image"
+                ></div>
+                <div class="content-block">
+                  <h3 :class="{ external: [program.link].includes('http') }">{{ program.title }}</h3>
+                  <p>{{ program.short_description }}</p>
+                </div>
+              </a>
             </div>
           </paginate>
         
-            <paginate-links for="programs"
-              :async="true"
-              :limit="3"
-              :show-step-links="true"
-              :hide-single-page="false"
-              :step-links="{
-                next: 'Next',
-                prev: 'Previous'
-              }"
-              @change="onLangsPageChange"
-              v-show="!loading && !emptyResponse && !failure">
-            </paginate-links>
-          </div>
+          <paginate-links
+            v-show="!loading && !emptyResponse && !failure"
+            for="programs"
+            :async="true"
+            :limit="3"
+            :show-step-links="true"
+            :hide-single-page="false"
+            :step-links="{
+              next: 'Next',
+              prev: 'Previous'
+            }"
+            @change="onLangsPageChange"
+          />
         </div>
-       
-    
+      </div>
     </div>
   </div>
 </template>
@@ -119,11 +126,11 @@ export default {
  
     getAllPrograms: function () {
   
-        console.log(philagov + programsEndpoint + 'archives')
+      console.log(philagov + programsEndpoint + 'archives');
       axios.get(philagov + programsEndpoint + 'archives' , {
         params: {
           'count': -1,
-        }
+        },
       })
         .then(response => {
           this.programs = response.data;
